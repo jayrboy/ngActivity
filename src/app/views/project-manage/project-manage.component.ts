7f03239845fe5ae7661bb5f88cf58c531b22b147
jayrboy <j.jayox_@hotmail.com>
2024-05-31 12:55:20 +0700

    fixed full update project

--- src/app/views/project-manage/project-manage.component.ts
+++ src/app/views/project-manage/project-manage.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormsModule, NgForm, Validators } from '@angular/forms';
+import { FormsModule, NgForm, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import Activity from '../../models/activity.model';
 import FileModel from '../../models/file.model';
@@ -35,18 +35,12 @@ import { MatListModule } from '@angular/material/list';
 export class ProjectManageComponent {
   projectId = -1;
   project = new Project();
-  newActivityName = '';
-  rows: number = 0;
 
   file: File[] = []; // Change to an array to store multiple files
-  file_url: string = '';
-  show_url: string = '';
-
-  showFiles: FileModel[] = [];
   file_name: string = '';
 
-  url_list: [] = [];
-  file_list: [] = [];
+  showFiles: FileModel[] = [];
+  file_list: File[] = [];
 
   @ViewChild('projectForm') form!: NgForm;
 
@@ -59,7 +53,7 @@ export class ProjectManageComponent {
     this.projectId = Number(this._route.snapshot.params['id']);
     this._projectService.getById(this.projectId).subscribe(
       (result: Response) => {
-        console.log(result);
+        // console.log(result.data);
 
         this.project = result.data;
         this.showFiles = result.data.file;
@@ -70,64 +64,53 @@ export class ProjectManageComponent {
     );
   }
 
-  toggleNewActivityField() {
-    this.newActivityName = this.newActivityName ? '' : this.newActivityName;
-  }
-
   addActivity() {
-    if (this.newActivityName.trim() !== '') {
-      const newActivity: Activity = {
-        id: 0,
-        projectId: 0,
-        activityHeaderId: 0,
-        name: this.newActivityName,
-        createDate: new Date().toISOString(),
-        updateDate: new Date().toISOString(),
-        isDelete: false,
-        activityHeader: null,
-        inverseActivityHeader: [],
-        project: new Project(),
-      };
+    const newActivity: Activity = {
+      id: 0,
+      projectId: 0,
+      activityHeaderId: 0,
+      name: '',
+      createDate: new Date().toISOString(),
+      updateDate: new Date().toISOString(),
+      isDelete: false,
+      activityHeader: null,
+      inverseActivityHeader: [],
+      project: new Project(),
+    };
 
-      this.project.activities.push(newActivity);
-      this.newActivityName = '';
-    }
+    this.project.activities.push(newActivity);
   }
 
   addSubActivity(activity: Activity) {
-    if (this.newActivityName.trim() !== '') {
-      const subActivity: Activity = {
-        id: 0,
-        projectId: activity.projectId,
-        activityHeaderId: activity.id,
-        name: this.newActivityName,
-        createDate: new Date().toISOString(),
-        updateDate: new Date().toISOString(),
-        isDelete: false,
-        activityHeader: null,
-        inverseActivityHeader: [],
-        project: new Project(),
-      };
+    const subActivity: Activity = {
+      id: 0,
+      projectId: activity.projectId,
+      activityHeaderId: activity.id,
+      name: '',
+      createDate: new Date().toISOString(),
+      updateDate: new Date().toISOString(),
+      isDelete: false,
+      activityHeader: null,
+      inverseActivityHeader: [],
+      project: new Project(),
+    };
 
-      activity.inverseActivityHeader.push(subActivity);
-      this.newActivityName = '';
-    }
+    activity.inverseActivityHeader.push(subActivity);
   }
 
   // remove this is a activity
   removeSubActivity(activity: Activity): void {
     // console.log('Deleted :', activity.name);
+
     activity.isDelete = true;
 
-    // activity.inverseActivityHeader.map((act) => {
-    //   act.isDelete = true;
+    activity.inverseActivityHeader.map((act) => {
+      act.isDelete = true;
 
-    //   if (act.inverseActivityHeader != null) {
-    //     act.inverseActivityHeader.map((subAct) =>
-    //       this.removeSubActivity(subAct)
-    //     );
-    //   }
-    // });
+      if (act.inverseActivityHeader != null) {
+        act.inverseActivityHeader.map((sub) => this.removeSubActivity(sub));
+      }
+    });
   }
 
   //TODO:
@@ -135,46 +118,43 @@ export class ProjectManageComponent {
     let files: FileList = event.target.files; // Get the file list
     this.file = Array.from(files); // Convert FileList to an array
 
+    this.file.forEach((f) => {
+      // 1024 KiloBytes == 1 MegaBytes
+      if (f.size > 100 * 1024) {
+        event.target.value = null;
+        this.file_list = [];
+        this.file_name = '';
+        this._toastr.warning('ขนาดไฟล์ของไฟล์ต้องไม่เกิน 100 MB');
+      } else {
+        this.file_list.push(f);
+      }
+    });
+
     if (files.length > 5) {
       this._toastr.warning('กรุณาเพิ่มไฟล์ได้ไม่เกินครั้งละ 5 ไฟล์');
       this.file_name = '';
-      this.file = [];
+      this.file_list = [];
       event.target.value = null;
-    } else {
-      this.file.map((f, i) => {
-        if (f.size > 100 * 1024) {
-          this.file_name = '';
-          this._toastr.warning('ขนาดไฟล์ของไฟล์ต้องไม่เกิน 100 KB');
-          this.file = [];
-          event.target.value = null;
-        }
-      });
-    }
-
-    if (files.length > 0) {
-      this.show_url = URL.createObjectURL(files[0]); // Show URL for the first file as an example
+    } else if (this.file_list.length > 5) {
+      event.target.value = null;
+      this.file_list = [];
+      this._toastr.warning('กรุณาเพิ่มไฟล์ได้ไม่เกิน 5 ไฟล์');
+    } else if (files.length > 0) {
       this.file_name = files[0].name; // Show name of the first file as an example
     }
   }
 
   onSubmit() {
     // console.log(this.project);
-    // console.log(this.file);
+    // console.log(this.file_list);
 
     if (this.form.valid) {
-      this._projectService.putFormData(this.project, this.file).subscribe(
+      this._projectService.putFormData(this.project, this.file_list).subscribe(
         (result: Response) => {
           // console.log(result.data);
 
-          if (this.file.length > 0) {
-            // loop files
-            this.file.forEach((f) => {
-              const fileUrl = this._projectService.download(f);
-            });
-          }
-
           this._router.navigate(['/']).then(() => {
-            this._router.navigate(['/manage', this.projectId]);
+            this._router.navigate(['/project/manage', this.projectId]);
             this._toastr.success('อัปเดตข้อมูลสำเร็จ');
           });
         },
@@ -185,12 +165,7 @@ export class ProjectManageComponent {
     }
   }
 
-  onDownload(file: any) {
-    console.log(file);
-    // this._projectService.downloadV2(file);
-  }
-
-  onIsDelete(id: any) {
+  onIsDelete(id: number) {
     const confirmDelete = confirm('ยืนยันลบรายการนี้?');
 
     if (confirmDelete) {
